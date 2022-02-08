e67ffde6cd68282d76add5668ef59f8eb11701be
David Gomes <davidbgomes92@gmail.com>
2022-02-08 17:59:49 +0000

    Delete GA tag and use Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import "@fontsource/faustina"
 
 import Header from "../components/Header";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -51,16 +51,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <GdprBanner />
       </ChakraProvider>
-      {/* <!-- Global site tag (gtag.js) - Google Analytics -->  */}
+      {/* <!-- Google Tag Manager -->  */}
       <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-      />
-      <Script
-        id="googleAnalytics"
+        id="googleTagManager"
         async
         dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_ID}');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}')`,
         }}
       />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ import Document, {
   NextScript,
 } from "next/document";
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -19,6 +21,11 @@ class MyDocument extends Document {
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9812155664610860" crossOrigin="anonymous"></script>
         </Head>
         <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+          </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
